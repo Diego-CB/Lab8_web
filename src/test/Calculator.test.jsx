a11a68dbbda9d8e117728584a0b70ec49b9a9956
Nes15442 <60157586+Nes15442@users.noreply.github.com>
2022-05-19 12:18:22 -0600

    Avances en testing
    Se agregaron los primeros 4 tests

--- src/test/Calculator.test.jsx
+++ src/test/Calculator.test.jsx
@@ -4,13 +4,77 @@ import '@testing-library/jest-dom'
 import userEvent from '@testing-library/user-event'
 import Calculator from '../components/Calculator'
 
-it('renders', () => {
-  render(<Calculator />)
-
-  const number5 = screen.getByText(/5/)
-  userEvent.click(number5)
+describe('When a number button is pressed', () => {
+  it('shows the number in the display', async () => {
+    render(<Calculator />)
+    const display = (await screen.findAllByText(/0/))[0]
+    const button5 = await screen.findByText(/5/)
+    const button2 = await screen.findByText(/2/)
   
-  const numberOf5 = screen.getAllByText(/5/).length
-  screen.debug()
-  expect(numberOf5).toBe(2)
+    await userEvent.click(button5)
+    await userEvent.click(button2)
+
+    expect(
+      display.innerHTML
+    ).toBe('52')
+  })
+})
+
+describe('When a number with more than 9 digits is typed', () => {
+  it('shows ERROR message in the display', async () => {
+    render(<Calculator />)
+    const button5 = await screen.findByText(/5/)  
+
+    for (let i = 0; i < 10; i++){
+      await userEvent.click(button5)
+    }
+
+    expect(
+      await screen.findByText(/ERROR/)
+    ).toBeInTheDocument()
+  })
+})
+
+describe('When the = button is pressed after an operation is typed', () => {
+  it('shows the result in the display', async () => {
+    render(<Calculator />)
+    
+    const display = (await screen.findAllByText(/0/))[0]
+    const button5 = await screen.findByText(/5/)
+    const button2 = await screen.findByText(/2/)
+    const less = await screen.findByText(/-/)
+    const equal = await screen.findByText(/=/)
+  
+    // Simulacion de operacion: 5-2
+    await userEvent.click(button5)
+    await userEvent.click(less)
+    await userEvent.click(button2)
+    await userEvent.click(equal)
+
+    expect(
+      display.innerHTML
+    ).toBe('3')
+  })
+})
+
+describe('When an operation with negative result is typed', () => {
+  it('shows ERROR message in the display', async () => {
+    render(<Calculator />)
+    
+    const display = (await screen.findAllByText(/0/))[0]
+    const button5 = await screen.findByText(/5/)
+    const button2 = await screen.findByText(/2/)
+    const less = await screen.findByText(/-/)
+    const equal = await screen.findByText(/=/)
+  
+    // Simulacion de operacion: 2-5
+    await userEvent.click(button2)
+    await userEvent.click(less)
+    await userEvent.click(button5)
+    await userEvent.click(equal)
+
+    expect(
+      display.innerHTML
+    ).toBe('ERROR')
+  })
 })
